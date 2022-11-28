bd0305f3e238262e8f7397b1c148b42ccc2eb0ec
Djed <djeday123@gmail.com>
2022-11-28 13:04:16 +0400

    first comit

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/* const myModule = require('./main');
+
+const myModuleInstance = new myModule();
+
+myModuleInstance.hello();
+myModuleInstance.goodbye();
+*/
+{
+    var name = 'gg';
+}
+console.log(name);
